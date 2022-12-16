f7211450af99f525563af93a433a9f7cfa4c6452
Mevlut Doger <mdoger@hotmail.com>
2022-12-16 13:47:15 +0000

    Wrote the code for the first class

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,5 +4,10 @@ public class First {
     public static void main(String[] args) {
         System.out.println("First");
         System.out.println("First Class");
+        int sum=0;
+        for (int i= 0; i < 13; i++) {
+            sum+=i;
+        }
+        System.out.println(sum);
     }
 }
